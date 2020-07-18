b665bb0b2d69d565b089365ad64bab9e1d541c07
vsezol <vsevolod.zolotov02@mail.ru>
2020-07-18 16:11:06 +0700

    write hexa2rgba func

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="message">
+    <div
+      class="message__avatar rounded-circle"
+      :style="{ background: color }"
+    ></div>
+    <div class="message__main"></div>
     <slot name="nick"></slot>
     <slot name="text"></slot>
   </div>
 </template>
 
+<script>
+export default {
+  props: ['color']
+}
+</script>
+
 <style lang="sass" scoped>
 .message
   color: white
+
+  &__avatar
+    width: 32px
+    height: 32px
 </style>
--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -1,9 +1,22 @@
+const hexa2rgba = hexa => {
+  let rgba = 'rgba('
+  for (let i = 1; i < 9; i += 2) {
+    const byte = hexa.slice(i, i + 2)
+    if (i !== 7) {
+      rgba += parseInt(byte, 16) + ','
+    } else {
+      rgba += (parseInt(byte, 16) / 255).toFixed(2) + ')'
+    }
+  }
+  return rgba
+}
+
 const login = {
   namespaced: true,
   state: {
     // for dev
     isLogged: true,
-    color: '#FF00AAFF',
+    color: '',
     nick: 'vsezol'
     // for prod
     // isLogged: false,
@@ -13,7 +26,7 @@ const login = {
   mutations: {
     login(state, { color, nick }) {
       state.isLogged = true
-      state.color = color
+      state.color = hexa2rgba(color)
       state.nick = nick
     },
     logout(state) {
